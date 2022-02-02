be46f670b4f0b993e565303b26f0b9b887e0ef31
Nathan Kelete <nathanfk.ad.unc>
2022-02-02 14:01:18 -0500

    Completed Exercise 2

--- exercises/ex02_one_shot_wordle2.py
+++ exercises/ex02_one_shot_wordle2.py
@@ -10,29 +10,42 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
+"""Asking user for their guess"""
 
-user_inputed_word: str = input("What is your 6-letter guess? ")
+user_inputed_word: str = input(f"What is your {len(secret_word)}-letter guess? ")
 
+"""Making sure the user types the amount of letters specified"""
 
-while len(user_inputed_word) != len(secret_word):
-    user_inputed_word = input("That was not 6 letters! Try again: ")
+while len(user_inputed_word) != len(secret_word): 
+    user_inputed_word = input(f"That was not {len(secret_word)} letters! Try again: ")
 
-while checking_indices < len(secret_word):
-    if user_inputed_word[checking_indices] == secret_word[checking_indices]:
-        guess_emojis = guess_emojis + GREEN_BOX
-    else:
-        yellow_characters: bool = False
-        alternate_indices = 0
-        while yellow_characters is not True and alternate_indices < len(secret_word):
-            if secret_word[alternate_indices] == user_inputed_word[checking_indices]:
-                yellow_characters = True
-            else:
-                alternate_indices = alternate_indices + 1
-        if yellow_characters is True:
-            guess_emojis = guess_emojis + YELLOW_BOX
+while user_inputed_word != secret_word:
+    while checking_indices < len(secret_word):
+        """Telling the user which letters are in the correct spot"""
+        guess_emojis = " "  
+        if user_inputed_word[checking_indices] == secret_word[checking_indices]:
+            guess_emojis = guess_emojis + GREEN_BOX
         else:
-            guess_emojis = guess_emojis + WHITE_BOX
-    checking_indices += 1
+            yellow_characters: bool = False
+            alternate_indices = 0
+            while yellow_characters is not True and alternate_indices < len(secret_word):
+                """Making note of which letters of the user's word are in the secret word, but in the wrong spot"""
+                if secret_word[alternate_indices] == user_inputed_word[checking_indices]:
+                    yellow_characters = True
+                else:
+                    """Making sure the loop isn't infinite"""
+                    alternate_indices = alternate_indices + 1
+            if yellow_characters is True:
+                """Printing which letters specifically are in the wrong spot but in the secret word"""
+                guess_emojis = guess_emojis + YELLOW_BOX
+            else:
+                """Printing which letters are not in the secret word at all"""
+                guess_emojis = guess_emojis + WHITE_BOX
+        """Making sure the original loop isn't infinite"""
+        checking_indices += 1
+    if user_inputed_word != secret_word:
+        print(guess_emojis)
+        user_inputed_word = input("That's incorrect! What's your next guess? ")
 print(guess_emojis)
 
 if user_inputed_word != secret_word:
